772747c2094adadc65f40557a0f26b82da375e99
Maddy Delos Reyes <maddydr@ucla.edu>
2023-11-30 00:44:34 -0800

    added feature to rank priority of a task, pick a due date, and updated search feature to show message when no results match query

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,4 +1,21 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
 export default function Task({ name, complete, id }) {
+
+  const [priority, setPriority] = useState('');
+  const [dueDate, setDueDate] = useState(null);
+
+  const handlePriorityChange = (e) => {
+    setPriority(e.target.value);
+  };
+
+  const handleDateChange = (date) => {
+    setDueDate(date);
+  };
+
+
   return (
     <>
       <div className="task">
@@ -7,6 +24,22 @@ export default function Task({ name, complete, id }) {
           <span className="custom-checkbox"></span>
           {name}
         </label>
+
+        {/* Dropdown menu for task priority */}
+        <select value={priority} onChange={handlePriorityChange}>
+          <option value="">Select Priority</option>
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+
+
+        {/* Date picker for task due date */}
+        <DatePicker
+            selected={dueDate}
+            onChange={handleDateChange}
+            placeholderText="Select Due Date"
+        />
       </div>
     </>
   );
