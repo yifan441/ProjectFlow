0bcdbc8656cef0d2f3cdbd36df8924cb746b110e
Raj Jain <raj.jain@mergingelements.com>
2023-11-30 19:47:39 -0800

    Created calendar component, awaiting due date data for updating

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -1,5 +1,24 @@
-export default function Calendar({ projectsData }) {
-  return <h3>Calendar</h3>;
-}
+import React from 'react';
+import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-// projectsData --> array of user data
+const localizer = momentLocalizer(moment);
+
+export default function Calendar() {
+  return (
+    <div className="calendar-page-container">
+      <h1>Calendar</h1>
+      <div style={{ height: 500 }}>
+        <BigCalendar
+          localizer={localizer}
+          events={[]} // Pass an empty array to display an empty calendar
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: '100%' }}
+          // Other props as needed
+        />
+      </div>
+    </div>
+  );
+}
