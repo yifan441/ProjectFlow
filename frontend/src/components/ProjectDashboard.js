c1fafa8c833cd7e781daba3e9cbd9070de9b2b1a
Maddy Delos Reyes <maddydr@ucla.edu>
2023-12-03 19:38:47 -0800

    made a search icon near search bar

--- frontend/src/components/ProjectDashboard.js
+++ frontend/src/components/ProjectDashboard.js
@@ -49,7 +49,22 @@ export default function ProjectDashboard({
         <div className="project-display-header-div">
           <h1>{projectName}</h1>
           <div>
-            Search:
+           {/* Static magnifying glass figure */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-search" // Add a class for styling if needed
+            >
+              <circle cx="11" cy="11" r="8" />
+              <line x1="21" y1="21" x2="16.65" y2="16.65" />
+            </svg>
             <input
               value={query}
               onChange={(e) => setQuery(e.target.value)}
