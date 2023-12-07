9256cfc503a357ac06d9c7c9cfd47f4bcd2b6d09
Yifan Tang <yifant@g.ucla.edu>
2023-12-06 18:11:22 -0800

    Added DashboardNavigationPanel header styles

--- frontend/src/components/ProjectNavigationPanel.js
+++ frontend/src/components/ProjectNavigationPanel.js
@@ -77,7 +77,6 @@ export default function ProjectNavigationPanel({
         const pdfData = addIdToJsonString(response.data.result);
         // console.log((JSON.stringify(pdfData)));
         handleProjectAdd(pdfData);
-        
       } catch (error) {
         console.log('Error with OpenAI Request.', error.message);
         console.log('dispatching errorAI');
@@ -144,8 +143,21 @@ export default function ProjectNavigationPanel({
         </div>
         <div className="new-project">
           <form action="" className="new-project-form" onSubmit={handleSubmit}>
-            <button type="submit" className="btn-create" aria-label="create new project">
-              +
+            <button type="submit" className="create-project-btn" aria-label="create new project">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="22"
+                height="22"
+                viewBox="0 0 22 22"
+                fill="none"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M21.5 11C21.5 16.799 16.799 21.5 11 21.5C5.20101 21.5 0.5 16.799 0.5 11C0.5 5.20101 5.20101 0.5 11 0.5C16.799 0.5 21.5 5.20101 21.5 11ZM11 17.8333C10.4477 17.8333 10 17.3856 10 16.8333V12H5.16667C4.61438 12 4.16667 11.5523 4.16667 11C4.16667 10.4477 4.61438 10 5.16667 10H10V5.16667C10 4.61438 10.4477 4.16667 11 4.16667C11.5523 4.16667 12 4.61438 12 5.16667V10H16.8333C17.3856 10 17.8333 10.4477 17.8333 11C17.8333 11.5523 17.3856 12 16.8333 12H12V16.8333C12 17.3856 11.5523 17.8333 11 17.8333Z"
+                  fill="#626060"
+                />
+              </svg>
             </button>
             <input
               type="text"
@@ -153,6 +165,7 @@ export default function ProjectNavigationPanel({
               onChange={handleChange}
               placeholder="New project"
               aria-label="New project"
+              className="create-project-input"
             />
           </form>
         </div>
