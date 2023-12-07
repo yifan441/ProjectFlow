40207904c5c0c3349a2e2d51c33c9fbd6a9f5be7
Yifan Tang <yifant@g.ucla.edu>
2023-12-07 15:52:34 -0800

    Readded import bootstrap and added minor edits

--- frontend/src/components/PasswordSuitability.js
+++ frontend/src/components/PasswordSuitability.js
@@ -3,24 +3,32 @@ import React, { useEffect } from 'react';
 //Informs registering users when they attempt to register with a password that does not meet requisites.
 //Delete comments after merging.
 const PasswordSuitability = () => {
-    useEffect(() => {
-        const handleUnsuitablePasswordEvent = () => {
-            const passwordMessage = document.getElementById('passwordMessage');
-            passwordMessage.style.display = 'block';
-        };
+  useEffect(() => {
+    const handleUnsuitablePasswordEvent = () => {
+      const passwordMessage = document.getElementById('passwordMessage');
+      passwordMessage.style.display = 'block';
+    };
 
-        document.addEventListener('unsuitablePassword', handleUnsuitablePasswordEvent);
+    document.addEventListener('unsuitablePassword', handleUnsuitablePasswordEvent);
 
-        return () => {
-            document.removeEventListener('unsuitablePassword', handleUnsuitablePasswordEvent);
-        };
-    }, []);
+    return () => {
+      document.removeEventListener('unsuitablePassword', handleUnsuitablePasswordEvent);
+    };
+  }, []);
 
-    return (
-        <div id="passwordMessage" style={{ display: 'none', color: 'red', fontSize: '10px' }}>
-            Password does not meet specified requirements.
-        </div>
-    );
+  return (
+    <div
+      id="passwordMessage"
+      style={{
+        display: 'none',
+        color: 'red',
+        fontSize: '10px',
+        marginLeft: '40px',
+      }}
+    >
+      Password does not meet specified requirements.
+    </div>
+  );
 };
 
 export default PasswordSuitability;
